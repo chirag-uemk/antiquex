6515e49dc9f46159699b5f07f766d5a5c3b8f2a8
chirag-uemk <140333955+chirag-uemk@users.noreply.github.com>
2024-01-01 04:13:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './NavbarSimple'
+import Carousel from './Carousel'
+import Footer from './Footer'
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+function App() {
+  return (
+    <div className="App">
+      <h1>Babletali</h1>
+      <Navbar/>
+      <Carousel images={images}/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
